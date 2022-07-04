05b38762a390171259c18690cc67a678ab146430
Marmisha <marmisha1991@rambler.ru>
2022-07-04 18:49:05 +0300

    HW4.1 done

---
+++ HW4.1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//Создаем метод, макрос:
+int MethodIntReadLine(string message)
+{
+    Console.Write(message);
+    string number = Console.ReadLine();
+    int numInt = int.Parse(number);
+    return numInt;
+}
+
+// Включаем работу метода (дважды):
+int numA = MethodIntReadLine("Введите два числа. Первое: ");
+int numB = MethodIntReadLine("Второе: ");
+Console.Write("Ваши числа: " + numA + " и " + numB + ".");
+
+//Возводим в степень:
+int i = 0;//счетчик
+int degree = 1;//степень, пока она равна 1
+for (i = 1; i <= numB; i++) //пока значение степени меньше значения второго числа, продолжаем умножать первое число на себя 
+{
+    degree = degree * numA;//поскольку нам надо хранить где-то значение, мы считаем в ddegree.
+
+}
+Console.WriteLine();
+Console.WriteLine($"Итак, {numA} в степени {numB} равно {degree}.");
---
+++ HW4.2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//Console.Write("Привет, напиши какое-нибудь число, лучше длинное: ");
+
+int MethodIntReadLine(string message)
+{
+    Console.Write(message);
+    string number = Console.ReadLine();
+    int numInt = int.Parse(number);
+    return numInt;
+} // Наша функция предлагает ввести число, съедает его в виде текста и превращает в integer
+
+
+int number = MethodIntReadLine("Введите число: ");
+Console.WriteLine($"Вы ввели {number}.");
+
+int[,]matrix = new int [1,number];
+Console.Write(matrix);
