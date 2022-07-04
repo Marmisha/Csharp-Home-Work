d34def359f8f18f82641d92ad59dc9447cec56cc
Marmisha <marmisha1991@rambler.ru>
2022-07-04 20:42:22 +0300

    HW1.4 reloaded

---
+++ HW1.4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.Write("Привет! Напиши число: ");
+string numberN = Console.ReadLine();
+int numberNInt = int.Parse(numberN);
+Console.WriteLine($"Итак, наше число равно: {numberN}, получаем интервал: от 1 до {numberN}.");
+int result = numberNInt / 2;
+int control = result * 2;
+
+ int countNumber = 1+1; // первое четное, исходя из условия
+ Console.Write("Четные числа из заданного интервала: ");
+ while (countNumber <= numberNInt)
+        {
+        Console.Write($"{countNumber} ");
+        countNumber = countNumber +2;
+        };
+Console.Write(".");
