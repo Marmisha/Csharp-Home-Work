4a657fb51046317b06a02d15c82a28d031067fa7
Marmisha <marmisha1991@rambler.ru>
2022-07-04 20:40:51 +0300

    HW2.1 reloaded

---
+++ HW2.1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру
+// этого числа.
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+string num = Console.ReadLine();
+int numInt  = int.Parse(num);
+//Console.WriteLine(num);
+int numdiv1 = numInt/10;
+//Console.WriteLine(numdiv1);
+int numdiv2 = numdiv1%10;
+Console.WriteLine($"Вторая цифра числа: {numdiv2}");
