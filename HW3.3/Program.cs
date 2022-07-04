6d83ff2b16c1581deab7ae6c81f2a7e8bf8de13d
Marmisha <marmisha1991@rambler.ru>
2022-07-04 20:52:41 +0300

    HW3.3 reloaded

---
+++ HW3.3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 3: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27.
+//5 -> 1, 8, 27, 64, 125
+
+//3 было на семинаре или на лекции
+ 
+ Console.Clear();
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string param = Console.ReadLine();
+    return int.Parse(param);// Иными словами: что мы введем, вернется как int.
+}
+int N = Prompt("Введите число N: ");
+Console.WriteLine($"Вы ввели число {N}. Наши кубы:");
+
+int i = 0; 
+
+while (i < N)
+{
+    i++;
+     Console.WriteLine(i * i * i);
+}
