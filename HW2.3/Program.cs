5844fb979856b538fc214a686aa2be0a2968ef0b
Marmisha <marmisha1991@rambler.ru>
2022-07-04 20:45:43 +0300

    HW2.3 reloaded

---
+++ HW2.3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+string num = Console.ReadLine();
+int numInt = int.Parse(num);
+Console.WriteLine("Наше число: " + numInt);
+
+if (numInt<100 && numInt>-100)
+{
+    Console.WriteLine("Неверно, число имеет меньше 3 разрядов, третьей цифры нет, придумайте другое");
+    
+}
+else
+{
+    while(numInt>999 || numInt<-999) 
+    {
+    numInt = numInt/10;
+    }
+    Console.WriteLine($"Сокращенное до трех разрядов число: {numInt}");
+    int thirdNum = numInt%10;
+    Console.WriteLine($"Третья цифра числа: {thirdNum}");
+}
