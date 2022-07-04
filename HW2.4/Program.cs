1df7e6033168a9ff79f94c950e8111a44c9ef74b
Marmisha <marmisha1991@rambler.ru>
2022-07-04 20:47:46 +0300

    HW2.4 reloaded

---
+++ HW2.4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Clear();
+Console.WriteLine("Введите число от 1 до 7");
+string num = Console.ReadLine();
+int numInt = int.Parse(num);
+Console.WriteLine("Наше число " + numInt + ".");
+if (numInt > 7 || numInt < 1)
+{
+    Console.WriteLine("Неправильное число, придумайте другое");
+}
+else 
+
+    if (numInt == 6 || numInt == 7)
+    {
+    Console.WriteLine("Ура, это выходной!");
+    }
+    else
+    {
+    Console.WriteLine("Улыбаемся и пашем");
+    }
