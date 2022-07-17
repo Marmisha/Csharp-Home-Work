04dd870a7c27b09688001dbdd31a0a6834222dd3
Marmisha <marmisha1991@rambler.ru>
2022-07-17 14:25:49 +0300

    HW6.1 done

---
+++ HW6.1/Program.cs
@@ -0,0 +1,39 @@
+﻿//HW6.1 Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+int GetNumFromReadLine(string msg) // получение интеджера из введенного ползователем текса
+{
+    Console.Write(msg);
+    int totalQuantityOfNumbers = int.Parse(Console.ReadLine());
+    return totalQuantityOfNumbers;
+}
+
+
+
+int[] InputNumbers(int M) // Получение заданного количества чисел от пользователя
+{
+    int[] numbers = new int[M];
+    for (int i = 0; i < M; i++)
+    {
+        numbers[i] = GetNumFromReadLine($"Введите {i + 1}-е число из {M}: ");
+    }
+    return numbers;
+}
+
+
+
+
+int CountOfPositiveElements(int[] array)
+{
+    int counter = 0;
+    foreach (int el in array)
+    {
+        if (el > 0) 
+        counter++;
+    }
+    return counter;
+}
+
+int M = GetNumFromReadLine("Сколько чисел будем вводить? => ");
+int[] myNumbers = InputNumbers(M);
+System.Console.WriteLine($"Число положительных элементов = {CountOfPositiveElements(myNumbers)}");
---
+++ HW6.2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2. Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Clear();
+
+
+double GetNumFromReadLine(string msg)
+{
+    Console.Write(msg);
+    double number = double.Parse(Console.ReadLine());
+    return number;
+}
+
+double b1 = GetNumFromReadLine("Введите значение для b1: ");
+double k1 = GetNumFromReadLine("Введите значение для k1: ");
+double b2 = GetNumFromReadLine("Введите значение для b2: ");
+double k2 = GetNumFromReadLine("Введите значение для k2: ");
+double x = GetNumFromReadLine("Введите значение для x: ");
+double coordinate1 = k1 * x + b1;
+double coordinate2 = k2 * x + b2;
+
+System.Console.WriteLine($"Пересечение линий будет в координате: {coordinate1}, {coordinate2}");
